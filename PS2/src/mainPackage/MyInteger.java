46f784852ef24f0dd8da577ce22a4ae3ebb6bb48
Michael Cisternino <mcistern@udel.edu>
2015-09-16 20:58:28 -0400

    update

--- PS2/src/mainPackage/MyInteger.java
+++ PS2/src/mainPackage/MyInteger.java
@@ -3,33 +3,39 @@
  * September 16, 2015
  */
 
-
 package mainPackage;
 
 // class MyInteger contains all methods, objects, and constructors for MyInteger
 public class MyInteger {
-	
-	public static void main(String[] args) {
-        //created this class to test MyInteger class and methods
-        
-        MyInteger newInt = new MyInteger(9);
-        System.out.println(newInt.isEven());
-        System.out.println(newInt.isOdd());
-        System.out.println(newInt.equals(9));
-        System.out.println(newInt.isPrime());
-        System.out.println(parseInt("1234"));
 
-    }
-	// an int data field named value that stores the int value represented by this object
+	public static void main(String[] args) {
+		// created this class to test MyInteger class and methods
+
+		char[] numbers = { '1', '2', '3', '4', '9' };
+		MyInteger newInt = new MyInteger(6);
+		System.out.println(newInt.isEven());
+		System.out.println(newInt.isOdd());
+		System.out.println(newInt.equals(9));
+		System.out.println(newInt.isPrime());
+		System.out.println(parseInt("1234"));
+		System.out.println(parseInt(numbers));
+
+	}
+
+	// an int data field named value that stores the int value represented by
+	// this object
 	private int value;
+
 	// a constructor that creates a MyInteger object for specified int value
 	public MyInteger(int num) {
 		value = num;
 	}
+
 	// getMyInteger returns int value
 	public int getMyInteger() {
 		return value;
 	}
+
 	// isEven returns true if given value is even
 	public boolean isEven() {
 		if (this.getMyInteger() % 2 == 0) {
@@ -38,6 +44,7 @@ public class MyInteger {
 			return false;
 		}
 	}
+
 	// isOdd returns true if given value is odd
 	public boolean isOdd() {
 		if (this.getMyInteger() % 2 != 0) {
@@ -46,6 +53,7 @@ public class MyInteger {
 			return false;
 		}
 	}
+
 	// is prime returns true if given value is prime
 	public boolean isPrime() {
 		int num = 0;
@@ -58,10 +66,50 @@ public class MyInteger {
 			return true;
 		}
 	}
+
 	// returns true if specified value is even
 	public static boolean isEven(int value) {
-		if (value % 2 == 0)
+		if (value % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// returns true if specified value is odd
+	public static boolean isOdd(int value) {
+		if (value % 2 != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// returns true if specified value is prime
+	public static boolean isPrime(int value) {
+		int num;
+		for (num = 0; num <= Math.sqrt(value); num++) {
+			if (value % 2 == 0) {
+				return false;
+			}
+		}
 		{
+			return true;
+		}
+	}
+
+	// returns true if specified value is even
+	public static boolean isEven(MyInteger myInt) {
+		if (myInt.getMyInteger() % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// returns true if specified value is odd
+	public static boolean isOdd(MyInteger myInt) {
+		if (myInt.getMyInteger() % 2 != 0) {
 			return true;
 		}
 		else
@@ -69,52 +117,57 @@ public class MyInteger {
 			return false;
 		}
 	}
-	// returns true if specified value is odd
-	public static boolean isOdd(int value) {
-		return isOdd(value);
-	}
-	// returns true if specified value is prime
-	public static boolean isPrime(int value) {
-		return isPrime(value);
-	}
-	// returns true if specified value is even
-	public static boolean isEven(MyInteger myInt) {
-		if (myInt.getMyInteger() % 2 == 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	// returns true if specified value is odd
-	public static boolean isOdd(MyInteger myInt) {
-		return MyInteger.isOdd(myInt.getMyInteger());
-	}
+
 	// returns true if specified value is prime
 	public static boolean isPrime(MyInteger myInt) {
-		return MyInteger.isPrime(myInt.getMyInteger());
-	}
-	// returns true if value in the object is equal to specified value
-	public boolean equals (int aValue) {
-		if (value == aValue) {
+		int num;
+		for (num = 0; num <= Math.sqrt(myInt.getMyInteger()); num++) {
+			if (myInt.getMyInteger() % 2 == 0) {
+				return false;
+			}
+		}
+		{
 			return true;
 		}
-		else {
+	}
+
+
+	// returns true if value in the object is equal to specified value
+	public boolean equals(int aValue) {
+		if (value == aValue) {
+			return true;
+		} else {
 			return false;
 		}
 	}
+
 	// returns true if value in the object is equal to specified value
-	public boolean equals (MyInteger myInt) {
-		return equals(myInt.getMyInteger());
+	public boolean equals(MyInteger myInt, int someValue) {
+		if (someValue == myInt.getMyInteger()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 	// parseInt converts a string into an int value
 	public static int parseInt(String x) {
-		return Integer.parseInt(x);
+		int i;
+		int sum = 0;
+		for (i = 0; i <= x.length(); i++) {
+			sum += i;
+		}
+		return sum;
 	}
+
 	// uses parseInt to convert an array of numeric characters into an int value
 	public static int parseInt(char[] anArray) {
-		
+		int i;
+		int sum = 0;
+		for (i = 0; i <= anArray.length; i++) {
+			sum += i;
+		}
+		return sum;
 	}
-	
-	
+
 }
